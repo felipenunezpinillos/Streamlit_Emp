53d6f540cf158df2170e30a6dca8b168d1a39e28
felipenunezpinillos <f.nunez@uniandes.edu.co>
2023-05-21 19:26:39 -0500

    creacion paginas

---
+++ pages/4_Que buscas con nosotros.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.title("¿Que buscas con nosotros?")
---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+# Creating a title using markdown syntax
+st.markdown("# Nexus App")
+# Rest of your Streamlit app code...
