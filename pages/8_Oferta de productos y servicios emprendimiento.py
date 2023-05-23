afef15160c625ddafbab46a4ece4008ecd0acf7d
felipenunezpinillos <f.nunez@uniandes.edu.co>
2023-05-22 19:06:50 -0500

    oferta p y s

--- pages/8_Oferta de productos y servicios emprendimiento.py
+++ pages/8_Oferta de productos y servicios emprendimiento.py
@@ -3,26 +3,33 @@ from streamlit_ace import st_ace
 import base64
 from streamlit_extras.switch_page_button import switch_page
 
-st.title("¿Qué tipos de emprendimientos buscas?")
-st. subheader("Añade una foto para tu perfil")
-# Crea dos columnas, una para el widget st_ace y otra para la imagen de perfil
-col1, col2 = st.columns([1, 1])
+st.title("¿Qué tipo de productos ofreces?")
 
-st. subheader("Buscar categoría por nombre")
+st. subheader("Buscar categorías de tus productos por nombre")
 
 # Opciones de categorías
-categorias = [
+categorias_productos = [
     "Tecnología",
     "Deportes",
     "Arte y cultura",
     "Moda",
     "Viajes",
+    "Ninguno",
+]
+
+categorias_servicios = [
+    "Tecnología",
+    "Deportes",
+    "Arte y cultura",
+    "Moda",
+    "Viajes",
+    "Ninguno",
 ]
 # Widget de checklist
-categorias_seleccionadas = st.multiselect("Selecciona tus categorías de interés", categorias)
+categorias_seleccionadas = st.multiselect("Selecciona las categorias de tus productos", categorias_productos, key='mt1')
 
 # Add custom CSS for dark mode and button styling
-custom_css = """
+custom_css1 = """
 <style>
 body {
     background-color: #222222;
@@ -38,29 +45,35 @@ body {
 """
 
 # Apply the custom CSS
-st.markdown(custom_css, unsafe_allow_html=True)
+st.markdown(custom_css1, unsafe_allow_html=True)
+
+st. subheader("Buscar categorías de tus servicios por nombre")
+
+# Widget de checklist
+categorias_seleccionadas = st.multiselect("Selecciona las categorias de tus servicios", categorias_servicios, key='mt2')
+
+# Add custom CSS for dark mode and button styling
+custom_css2 = """
+<style>
+body {
+    background-color: #222222;
+    color: #ffffff;
+}
+
+.stButton button {
+    display: block;
+    width: 100%;
+    margin: 0 auto;
+}
+</style>
+"""
+
+# Apply the custom CSS
+st.markdown(custom_css2, unsafe_allow_html=True)
+
 #Botones
-boton = st.button("Empezar a navegar", key="submit_button")
+boton = st.button("Siguiente", key="submit_button")
 if boton:
-    switch_page("Dashboard")
+    switch_page("imagenes emprendimiento")
     
-# Utiliza st_ace en la primera columna para crear el elemento de carga de archivos
-uploaded_code = col1.container()
 
-uploaded_file = uploaded_code.file_uploader("", type=['png', 'jpeg', 'jpg'], key="uploaded_file")
-
-# Verifica si se ha cargado un archivo y muestra la imagen de perfil en la segunda columna
-if uploaded_file is not None:
-    # Lee los datos de la imagen cargada
-    img_bytes = uploaded_file.read()
-
-    # Convierte los datos de la imagen a formato base64
-    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
-
-    # Muestra la imagen de perfil con estilo convencional utilizando HTML en la segunda columna
-    col2.markdown(
-        f'<style>.profile-pic {{ width: 150px; height: 150px; border-radius: 50%; object-fit: cover; }}</style>',
-        unsafe_allow_html=True
-    )
-    col2.markdown(f'<img class="profile-pic" src="data:image/png;base64,{img_base64}" alt="Foto de perfil">',
-                unsafe_allow_html=True)
